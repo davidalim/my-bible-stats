570e1503b283481172f467cd88c1080a070ee5db
David Lim <limda@amazon.com>
2019-05-05 21:57:23 -0700

    Fix scrolling

--- sidenav.js
+++ sidenav.js
@@ -1,42 +1,20 @@
-function openChaptersView() {
-    document.getElementById("books").style.width = "50%";
-    document.getElementById("chapters").style.width = "50%";
-    document.getElementById("chapters").style.left = "50%";
+function showBookSelectionView() {
+    $("#books").width("100%");
+    $("#chapters").width("0");
+    $("#verses").width("0");
 }
-function closeChaptersView() {
-    document.getElementById("chapters").style.width = "0";
-    openBooksView();
-}
-function openBooksView() {
-    document.getElementById("books").style.width = "100%";
-}
-function closeBooksView() {
-    document.getElementById("books").style.width = "0";
 
+function showChapterSelectionView() {
+    $("#books").width("50%");
+    $("#chapters").width("50%").css("left", "50%");
+    $("#verses").width("0%");
+    $("#chapters .closebtn").show();
 }
-function openChaptersView() {
-    document.getElementById("books").style.width = "50%";
-    document.getElementById("chapters").style.width = "50%";
-    document.getElementById("chapters").style.left = "50%";
-    closeVersesView();
+
+function showVersesView() {
+    $("#books").width("15%");
+    $("#chapters").width("15%").css("left", "15%");
+    $("#verses").width("70%").css("left", "30%");
+    $("#chapters .closebtn").hide();
 }
-function closeChaptersView() {
-    document.getElementById("chapters").style.width = "0";
-    openBooksView();
-}
-function openVersesView() {
-    // TODO: query verses from API
-    document.getElementById("verses").style.width = "70%";
-    minimizeBookChapterViews();
-}
-function closeVersesView() {
-    // TODO: do whatever needs to be done to close verse view
-    document.getElementById("verses").style.width = "0%";
-    openChaptersView();
-}
-function minimizeBookChapterViews() {
-    document.getElementById("books").style.width = "15%";
-    document.getElementById("chapters").style.width = "15%";
-    document.getElementById("chapters").style.left = "15%";
-    
-}
+
